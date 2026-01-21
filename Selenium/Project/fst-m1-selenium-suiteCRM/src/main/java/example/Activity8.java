83a431f884bae79fa016a9d1ce5ffb2ea50a1db7
prasadvas12 <161206442+prasadvas12@users.noreply.github.com>
2026-01-21 12:10:42 +0530

    Add files via upload

--- Selenium/Project/fst-m1-selenium-suiteCRM/src/main/java/example/Activity8.java
+++ Selenium/Project/fst-m1-selenium-suiteCRM/src/main/java/example/Activity8.java
@@ -1,33 +1,57 @@
 package example;
 
+import java.util.List;
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Activity8 {
 
 	public static void main(String[] args) {
 		WebDriver driver = new FirefoxDriver();
-		driver.get("https://alchemy.hguy.co/lms");
-		WebElement contact = driver.findElement(By.xpath("//a[contains(text(),'Contact')]"));
-		contact.click();
-	
+		driver.get("https://alchemy.hguy.co/crm");
 		
-		WebElement fName = driver.findElement(By.xpath("//label[contains(text(),'Full Name')]/parent::*//input"));
-		fName.sendKeys("prasad vasala");
-		WebElement email = driver.findElement(By.xpath("//label[contains(text(),'Email')]/parent::*//input"));
-		email.sendKeys("abc@gmail.com");
-		WebElement message = driver.findElement(By.xpath("//label[contains(text(),'Comment or Message')]/parent::*//textarea"));
-		message.sendKeys("hello we are from ibm");
-		WebElement submit = driver.findElement(By.xpath("//button[contains(text(),'Send Message')]"));
+		WebElement user = driver.findElement(By.id("user_name"));
+		user.sendKeys("admin");
+		WebElement pass = driver.findElement(By.id("username_password"));
+		pass.sendKeys("pa$$w0rd");
+		WebElement submit = driver.findElement(By.id("bigbutton"));
 		submit.click();
+//		Thread.sleep(10);
 		
-		WebElement afterSend = driver.findElement(By.id("wpforms-confirmation-8"));
-		String afterSendMessage = afterSend.getText();
-			System.out.println("User sent the message: " + afterSendMessage);
-			driver.quit();
+		Actions actions = new Actions(driver);
+		WebElement salesMenu = driver.findElement(By.xpath("//a[text()='Sales']"));
+		actions.moveToElement(salesMenu).perform();
 		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement acctsSubMenu = wait.until(
+		        ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Accounts']"))
+		);
+		
+		acctsSubMenu.click();
+		
+		List<WebElement> rows = driver.findElements(By.xpath("//*[@id='MassUpdate']/div[3]/table/tbody/tr"));
+		int count = 0;
+		for (int i = 1; i <= rows.size(); i += 2) {
+		    
+		    WebElement cell = driver.findElement(By.xpath("//*[@id='MassUpdate']/div[3]/table/tbody/tr["+i+"]/td[3]"));
+		    String name = cell.getText();
+		    
+		    System.out.println(name);
+		    count++;
+
+		    if (count == 5) {
+		        break;   // stop after first 5 odd names
+		        
+		    }
+		}
+		driver.quit();
 
 	}
 
